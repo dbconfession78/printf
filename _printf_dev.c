03185cb981c2f007b3d592cf8ceae2ce34edf6fb
Stuart <108@holbertonschool.com>
2017-03-14 20:26:43 +0000

    revert to day start

--- _printf_dev.c
+++ _printf_dev.c
@@ -7,13 +7,18 @@
 int _printf(char const *format, ...)
 {
 	va_list list;
-	char buffer[1024];
+	char *buffer;
+	char *temp_buffer;
+	size_t buffer_mult = 1;
 	int buffer_len = 0;
 	int (*func)(char *buffer, va_list) = NULL;
 	int skip;
 
 	va_start(list, format);
+	buffer = malloc(sizeof(char) * 1024);
+	temp_buffer = malloc(sizeof(char) * 1024);
 	reset_buffer(buffer, 1024);
+	reset_buffer(temp_buffer, 1024);
 
 	if (!format || (format[0] == '%' && strlen(format) == 1))
 		return (-1);
@@ -33,6 +38,17 @@ int _printf(char const *format, ...)
 		}
 		if (skip == 0)
 		{
+			getchar();
+			if (buffer_len == 1022 * buffer_mult)
+			{
+				strcpy(temp_buffer, buffer);
+				buffer_mult++;
+				buffer = realloc(&buffer, 1024 * buffer_mult);
+				reset_buffer(buffer,  1024 * buffer_mult);
+				strcpy(buffer, temp_buffer);
+				reset_buffer(temp_buffer, 1024);
+			}
+
 			buffer[buffer_len] = *format;
 			format++; buffer_len++;
 		}
