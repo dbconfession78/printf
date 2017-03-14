84dcac159a2e8c6f8a6e70df5775e2f173413a97
Stuart <108@holbertonschool.com>
2017-03-14 19:49:57 +0000

    handle buffer overflow

--- _printf.c
+++ _printf.c
@@ -7,12 +7,18 @@
 int _printf(char const *format, ...)
 {
 	va_list list;
-	char buffer[1024];
-	int buffer_len = 0;
+	char *buffer;
+	char *temp_buffer;
+	//char buffer[1024];
+
+	size_t buffer_len = 0;
+	int buffer_mult = 1;
 	int (*func)(char *buffer, va_list) = NULL;
 	int skip;
 
 	va_start(list, format);
+	buffer = malloc(sizeof(char) * 1024);
+	temp_buffer = malloc(sizeof(char) * 1024);
 	reset_buffer(buffer, 1024);
 
 	if (!format || (format[0] == '%' && strlen(format) == 1))
@@ -34,6 +40,16 @@ int _printf(char const *format, ...)
 		}
 		if (skip == 0)
 		{
+			if (buffer_len == 1022 * buffer_mult)
+			{
+				strcpy(temp_buffer, buffer);
+				//write(1, buffer, 1022 * buffer_mult);
+				buffer_mult++;
+				buffer = realloc(&buffer, 1024 * buffer_mult);
+				reset_buffer(buffer,  1024 * buffer_mult);
+				strcpy(buffer, temp_buffer);
+				reset_buffer(temp_buffer, 1024);
+			}
 			buffer[buffer_len] = *format;
 			format++; buffer_len++;
 		}
